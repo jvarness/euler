dbd3aed9e3f1ed51eba541de5401f5b6b119ffbe
Jake Varness <jake.varness@gmail.com>
2017-12-14 11:50:50 -0600

    Problems 18 and 67

---
+++ Problems11-20/Problem18/eighteen.dart
@@ -0,0 +1,40 @@
+import 'dart:math';
+
+main() {
+  const _tree = 
+'''75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23''';
+  var largestSum = 0;
+  var previousLine = [];
+  var lines = _tree.split('\n');
+  for (var line in lines) {
+    var numbers = line.split(' ').map((str) => int.parse(str)).toList();
+    if (numbers.length == 1) {
+      largestSum = numbers[0];
+    } else {
+      for (var x = 0; x < numbers.length; x++) {
+        var minPreviousIndex = max(0, x-1);
+        var maxPreviousIndex = x >= previousLine.length ? x-1 : x;
+        numbers[x] = max(numbers[x] + previousLine[minPreviousIndex], numbers[x] + previousLine[maxPreviousIndex]);
+      }
+    }
+    previousLine = numbers;
+    print('$numbers');
+    largestSum = numbers.reduce((x, y) => max(x, y));
+  }
+
+  print('Largest Sum: $largestSum');
+}
